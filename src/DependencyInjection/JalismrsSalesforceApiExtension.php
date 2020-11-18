a0ed982054d3e729a614a6fe3512225cb00dd618
kevin.castejon <kevin.castejon@jalis.fr>
2020-11-18 15:51:57 +0100

    clean config

--- src/DependencyInjection/JalismrsSalesforceApiExtension.php
+++ src/DependencyInjection/JalismrsSalesforceApiExtension.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 namespace Jalismrs\Symfony\Bundle\JalismrsSalesforceApiBundle\DependencyInjection;
 
+use Jalismrs\Symfony\Bundle\JalismrsApiThrottlerBundle\ApiThrottler;
+use Jalismrs\Symfony\Bundle\JalismrsSalesforceApiBundle\SalesforceApi;
+use Maba\GentleForce\RateLimit\UsageRateLimit;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -41,19 +44,33 @@ class JalismrsSalesforceApiExtension extends
         
         $yamlFileLoader->load('services.yaml');
     
-        $definition = $container->getDefinition(Configuration::CONFIG_ROOT . '.salesforce_api');
-        
-        $definition->replaceArgument(
-            '$username',
-            $mergedConfig['username']
+        $definition = $container->getDefinition(Configuration::CONFIG_ROOT . '.dependency.developerforce.force_com_toolkit_for_php.sforce_enterprise_client');
+        $definition->addMethodCall(
+            'createConnection',
+            [
+                '$wsdl' => __DIR__ . '../../salesforce.wsdl.xml',
+            ]
         );
-        $definition->replaceArgument(
-            '$password',
-            $mergedConfig['password']
+        $definition->addMethodCall(
+            'login',
+            [
+                '$username' => $mergedConfig['username'],
+                '$password' => $mergedConfig['password'] . $mergedConfig['token'],
+            ]
         );
-        $definition->replaceArgument(
-            '$token',
-            $mergedConfig['token']
+    
+        $definition = $container->getDefinition(ApiThrottler::class);
+        $definition->addMethodCall(
+            'registerRateLimits',
+            [
+                '$useCaseKey' => SalesforceApi::THROTTLER_KEY,
+                '$rateLimits' => [
+                    new UsageRateLimit(
+                        100000,
+                        60 * 60 * 24
+                    ),
+                ],
+            ]
         );
     }
 }
